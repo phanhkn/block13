0ade34a68a168e69e80bd1866bc7a6f9c6f40b2a
phanhkn <110194750+phanhkn@users.noreply.github.com>
2023-05-23 21:46:19 -0400

    Add files via upload

---
+++ block13.js
@@ -0,0 +1,54 @@
+// function evenOrOdd(int) {
+//   int % 2 === 0 ? console.log("even") : console.log("odd");
+// }
+// evenOrOdd(-5555555555555);
+
+// let num = 2;
+// if (num % 2 === 0) {
+//   console.log("even");
+// } else {
+//   console.log("odd");
+// }
+
+// let input = "";
+// if (input === "") {
+//   console.log("true");
+// } else {
+//   console.log("nah");
+// }
+
+// let num1 = 50;
+// let num2 = 20;
+// let sum = num1 + num2;
+
+// if (sum < -1000) {
+//   console.log(`${sum} is less than -1000`);
+// } else if (sum < 0) {
+//   console.log(`${sum} is a negative number`);
+// } else if (sum > 0) {
+//   console.log(`${sum} is greater than 0`);
+// } else if (sum === 0) {
+//   console.log(`${sum} is equal to 0`);
+// } else if (sum > 100) {
+//   console.log(`${sum} is greater than 100`);
+// }
+
+// let num1 = 7;
+// let num2 = 26;
+
+// if (num1 && num2 > 5) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
+
+// let param1A = "cat";
+// let param1B = 6;
+// let param2A = 6;
+// let param2B = "cat";
+
+// if (param1A === param1B || param2A === param2B) {
+//   console.log("true");
+// } else {
+//   console.log("false");
+// }
